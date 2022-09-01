a5cb2ed323d26f437e6276ad610976d50a027530
Mathieu Benoit <mathben@technolibre.ca>
2022-09-01 07:49:55 -0400

    remove im_livechat

--- addons/website_livechat/__manifest__.py
+++ addons/website_livechat/__manifest__.py
@@ -7,7 +7,7 @@
     'description': """
 Allow website visitors to chat with the collaborators. This module also brings a feedback tool for the livechat and web pages to display your channel with its ratings on the website.
     """,
-    'depends': ['website', 'im_livechat'],
+    'depends': ['website'],
     'installable': True,
     'application': True,
     'auto_install': True,
