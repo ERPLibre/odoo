441ecac1f7b5d0d926e3b0aa96868e874fa2ea59
Mathieu Benoit <mathben@technolibre.ca>
2022-10-18 04:46:37 -0400

    [FIX] website_events: valide event is not undefined before use it

--- addons/website_event/static/src/js/website_geolocation.js
+++ addons/website_event/static/src/js/website_geolocation.js
@@ -13,18 +13,20 @@ sAnimation.registry.visitor = sAnimation.Class.extend({
         var defs = [this._super.apply(this, arguments)];
         var self = this;
         var $eventList = this.$('.country_events_list');
-        this._originalContent = $eventList[0].outerHTML;
-        defs.push(this._rpc({route: '/event/get_country_event_list'}).then(function (data) {
-            if (data) {
-                self._$loadedContent = $(data);
+        let firstEvent = $eventList.first();
+        if (firstEvent !== undefined) {
+            this._originalContent = firstEvent.outerHTML;
+            defs.push(this._rpc({route: '/event/get_country_event_list'}).then(function (data) {
+                if (data) {
+                    self._$loadedContent = $(data);
+                    self._$loadedContent.attr('contentEditable', false);
+                    self._$loadedContent.addClass('o_temp_auto_element');
+                    self._$loadedContent.attr('data-temp-auto-element-original-content', self._originalContent);
 
-                self._$loadedContent.attr('contentEditable', false);
-                self._$loadedContent.addClass('o_temp_auto_element');
-                self._$loadedContent.attr('data-temp-auto-element-original-content', self._originalContent);
-
-                $eventList.replaceWith(self._$loadedContent);
-            }
-        }));
+                    $eventList.replaceWith(self._$loadedContent);
+                }
+            }));
+        }
         return $.when.apply($, defs);
     },
     /**
