afc23de60d3464b011d58f0f75f6671fd218c163
Mathieu Benoit <mathben@technolibre.ca>
2023-08-11 02:19:50 -0400

    [ADD] mail: field CC and TO to show CC in mail to other module mail_thread_cc: support email_to with same logic of email_cc - support ignore_mail to remove system mail from TO - add context when it's a mail.activity

--- addons/mail/models/mail_thread_cc.py
+++ addons/mail/models/mail_thread_cc.py
@@ -9,6 +9,7 @@ class MailCCMixin(models.AbstractModel):
     _inherit = 'mail.thread'
     _description = 'Email CC management'
 
+    email_to = fields.Char('Email to', help='List of to from incoming emails.')
     email_cc = fields.Char('Email cc', help='List of cc from incoming emails.')
 
     def _mail_cc_sanitized_raw_dict(self, cc_string):
@@ -18,12 +19,44 @@ class MailCCMixin(models.AbstractModel):
         return {tools.email_normalize(email): tools.formataddr((name, tools.email_normalize(email)))
             for (name, email) in tools.email_split_tuples(cc_string)}
 
+    def get_list_ignore_mail(self):
+        IRCP = self.env['ir.config_parameter'].sudo()
+        domain = IRCP.get_param("mail.catchall.domain")
+        lst_alias = [IRCP.get_param("mail.catchall.alias")]
+        lst_alias += [a.alias_name for a in self.env["mail.alias"].search([]) if a.alias_name]
+        lst_alias = list(set(lst_alias))
+        lst_email_ignore = [f'{alias}@{domain}' for alias in lst_alias]
+        return lst_email_ignore
+
+    def filter_email_by_remove_ignore_mail(self, emails):
+        if not emails:
+            return emails
+        lst_email_ignore = self.env[
+            "mail.thread.cc"
+        ].get_list_ignore_mail()
+        lst_emails = [
+            tools.formataddr((a, b))
+            for a, b in tools.email_split_tuples(emails)
+            if b not in lst_email_ignore
+        ]
+        return ", ".join(lst_emails)
+
     @api.model
     def message_new(self, msg_dict, custom_values=None):
         if custom_values is None:
             custom_values = {}
+
+        # Remove catchall email from TO
+        lst_email_ignore = self.get_list_ignore_mail()
+        lst_to = [tools.formataddr((a, b)) for a, b in tools.email_split_tuples(msg_dict.get('to')) if
+                  b not in lst_email_ignore]
+        to = ", ".join(lst_to)
+        lst_cc = [tools.formataddr((a, b)) for a, b in tools.email_split_tuples(msg_dict.get('cc')) if
+                  b not in lst_email_ignore]
+        cc = ", ".join(lst_cc)
         cc_values = {
-            'email_cc': ", ".join(self._mail_cc_sanitized_raw_dict(msg_dict.get('cc')).values()),
+            'email_cc': cc,
+            'email_to': to,
         }
         cc_values.update(custom_values)
         return super(MailCCMixin, self).message_new(msg_dict, cc_values)
@@ -43,8 +76,14 @@ class MailCCMixin(models.AbstractModel):
 
     def _message_get_suggested_recipients(self):
         recipients = super(MailCCMixin, self)._message_get_suggested_recipients()
+        lst_ignore_mail = self.get_list_ignore_mail()
         for record in self:
             if record.email_cc:
                 for email in tools.email_split_and_format(record.email_cc):
-                    record._message_add_suggested_recipient(recipients, email=email, reason=_('CC Email'))
+                    if tools.email_normalize(email) not in lst_ignore_mail:
+                        record._message_add_suggested_recipient(recipients, email=email, reason=_('CC Email'))
+            if record.email_to:
+                for email in tools.email_split_and_format(record.email_to):
+                    if tools.email_normalize(email) not in lst_ignore_mail:
+                        record._message_add_suggested_recipient(recipients, email=email, reason=_('TO Email'))
         return recipients
